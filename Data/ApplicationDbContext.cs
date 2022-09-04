f950b46068d5f5c8a34da8185f52312aafcda420
michelleVasquezA <michellevasquez2311@gmail.com>
2022-09-03 19:53:24 -0500

    contacto + login

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using HeladeriaTAMS.Models;
 
 namespace HeladeriaTAMS.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+    public DbSet<Contacto> DataContactos { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using HeladeriaTAMS.Data;
-using HeladeriaTAMS.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
