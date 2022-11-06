09eda1d43763bf6f65c85b97e54a8c2689b46af1
Tatiana <tatiana_florez@usmp.pe>
2022-11-05 21:59:59 -0500

    consumir servicios

---
+++ DTO/Users.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeladeriaTAMS.DTO.gorest
+{
+    public class Users
+    {
+        public int id {get;set;}
+        public string? name {get;set;}
+        public string? email {get;set;}
+        public string? gender {get;set;}
+
+    }
+}
